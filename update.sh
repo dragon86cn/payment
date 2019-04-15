afe7b27a938551070d825f6ceaf431d66556d582
dragon86cn <15698427+dragon86cn@users.noreply.github.com>
2019-04-15 14:28:19 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,3 @@
+sed -i "s#code.googlesource.com#github.com/googleapis#g" ./vendor/manifest
+sed -i "s#gocloud#google-cloud-go#g" ./vendor/manifest
+sed -i "s#go.googlesource.com#github.com/golang#g" ./vendor/manifest
